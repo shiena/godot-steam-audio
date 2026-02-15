88312de4443cad5b2165866f80b9d64745ec448c
KOGA Mitsuhiro <shiena.jp@gmail.com>
2026-02-15 17:42:57 +0900

    Add Embree fallback for iOS platform
    iOS does not support Embree, so fall back to the default scene type,
    consistent with the existing macOS ARM64 and Android fallbacks.

--- src/server_init.cpp
+++ src/server_init.cpp
@@ -85,6 +85,10 @@ IPLSceneSettings create_scene_cfg(IPLContext ctx) {
 			OS::get_singleton()->get_name() == "Android") {
 		SteamAudioConfig::scene_type = IPL_SCENETYPE_DEFAULT;
 		SteamAudio::log(SteamAudio::log_info, "Embree is not supported on Android, reverting to default scene type.");
+	} else if (SteamAudioConfig::scene_type == IPL_SCENETYPE_EMBREE &&
+			OS::get_singleton()->get_name() == "iOS") {
+		SteamAudioConfig::scene_type = IPL_SCENETYPE_DEFAULT;
+		SteamAudio::log(SteamAudio::log_info, "Embree is not supported on iOS, reverting to default scene type.");
 	}
 
 	scene_cfg.type = SteamAudioConfig::scene_type;
